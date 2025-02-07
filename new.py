3872423b3d5874b29ccd90eb13d6cc63b8892ae9
ShantoGUB567 <shahriarshanto221002567@gmail.com>
2025-02-08 01:11:01 +0600

    update file

--- new.py
+++ new.py
@@ -1,2 +1,44 @@
-
+import sys
 print("Hello Shahriar Shanto")
+
+print(sys.version)  #python version
+
+# Variable
+x = 5
+y = "Shanto"
+z = 10
+print(x)
+print(y)
+print(x,y)
+print(type(x))
+print(type(y))
+
+x = str(3) # convert integer to string. x = "3"
+print(type(x))
+
+a = 567
+print(a)
+a = 555
+print(a) # variable can be changed
+
+# Multiple Assign
+a, b, c = 1, 2, 3
+print(a)
+print(b)
+print(c)
+print(a,b,c)
+
+fruits = ["apple", "banana", "cherry"]
+x, y, z = fruits
+print(x)
+print(y)
+print(z)
+
+print("shanto")
+
+print(x,y,z)
+print(x + y + z)
+
+a = 230
+b = 320
+print(a + b)
