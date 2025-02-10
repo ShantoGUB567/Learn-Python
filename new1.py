125076337fabef419a23af2815071026bea30bf9
ShantoGUB567 <shahriarshanto221002567@gmail.com>
2025-02-11 00:53:57 +0600

    update file

---
+++ new1.py
@@ -0,0 +1,19 @@
+# for i in range(10):
+#     print(i)
+
+# for i in range(5, 10):
+#     print(i)
+
+# for i in range(0, 13, 3):
+#     print(i)
+
+name = "Shanto"
+for i in name:
+    print(i)
+
+name = ["Al", "Shahriar", "Ahommed ", "Shanto"]
+for n in name:
+    print(n)
+    for i in n:
+        print(i)
+
