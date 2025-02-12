43882b2e26e2bf1869f08667de4d5a38aa49884d
ShantoGUB567 <shahriarshanto221002567@gmail.com>
2025-02-13 00:54:37 +0600

    Update CLP

--- CLP-1.py
+++ CLP-1.py
@@ -11,3 +11,11 @@ print("Second highest number is: ", secondHighest)
 
 
 # [34,45,56,34,75,34,67,34,56,24,56,86,78] 
+
+
+# 1. Write a program that takes an input string of parentheses ()[]{} and determines whether the expression is balanced or not.
+# Input: [{[()]}]
+# Output: Balanced
+
+# Input: [{({}])}
+# Output: Not balanced
