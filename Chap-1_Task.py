69919907c26b3e64ded472fc82cd359391b5396c
ShantoGUB567 <shahriarshanto221002567@gmail.com>
2025-02-14 02:21:25 +0600

    Code With Harry Test file

---
+++ Chap-1_Task.py
@@ -0,0 +1,14 @@
+# Write a program to print Jony Jony Yes Papa poem in pythone
+print("""
+Johny Johny, yes papa?
+Eating sugar? No papa
+Telling lies? No papa
+Open your mouth
+Ha, ha, ha
+Johny Johny, yes papa?
+Eating sugar? No papa
+Telling lies? No papa
+Open your mouth
+Ha, ha, ha
+""")
+
