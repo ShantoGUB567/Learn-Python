10bd7ad09c59611ebd654465a9372078b4fcc7ec
ShantoGUB567 <shahriarshanto221002567@gmail.com>
2025-02-14 12:46:54 +0600

    update file

---
+++ DataType.py
@@ -0,0 +1,15 @@
+# 1. Numeric 
+# age = int(input("Enter age: "))  #int & float same vabe kaj kore
+# print(age)
+# id = int(input())
+# print(id)
+
+# real_p = int(input("Enter real part of complex number: "))
+# img_p = int(input("Enter imaginary part of complex number: "))
+# number = complex(real_p, img_p)        # complex number er 2 ta part alada kore input nite hoy
+# print(number)
+
+# 2. String
+name = input("Enter your name: ")
+print(name)
+
