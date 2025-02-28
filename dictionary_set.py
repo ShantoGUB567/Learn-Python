d98796ac30acf54d28f1006c80e83159fc008b3f
ShantoGUB567 <shahriarshanto221002567@gmail.com>
2025-03-01 01:08:08 +0600

    update file

--- dictionary_set.py
+++ dictionary_set.py
@@ -10,3 +10,12 @@ print (my_dict[12])
 print(my_dict)
 
 print(my_dict.get("city"))
+
+# Functions
+print(my_dict.keys())
+print(my_dict.values())
+print(my_dict.items())
+
+keys = ["name", "age", "city"]
+dict = dict.fromkeys(keys, "default")
+print(dict)
