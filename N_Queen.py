1fb7f538fb7baed2f37b955511a7109b9b558582
ShantoGUB567 <shahriarshanto221002567@gmail.com>
2025-04-30 01:08:59 +0600

    update file

--- N_Queen.py
+++ N_Queen.py
@@ -3,33 +3,28 @@ def print_board(board):
         print(" ".join(row))
     print()
 
-
 def is_safe(board, row, col, n):
-    # Check column
     for i in range(row):
-        if board[i][col] == 'Q':
+        if board[i][col] == "Q":
             return False
-
-    # Check upper left diagonal
-    i, j = row, col
-    while i >= 0 and j >= 0:
-        if board[i][j] == 'Q':
+    
+    i,j =row, col
+    while i>=0 and j>=0:
+        if board[i][j]== "Q":
             return False
-        i -= 1
-        j -= 1
-
-    # Check upper right diagonal
-    i, j = row, col
-    while i >= 0 and j < n:
-        if board[i][j] == 'Q':
+        i -=1
+        j -=1
+    
+    i,j =row, col
+    while i>=0 and j<n:
+        if board[i][j]== "Q":
             return False
-        i -= 1
-        j += 1
-
+        i -=1
+        j +=1
+    
     return True
 
-
-def solve_n_queens(board, row, n):
+def solve_n_queen(board, row, n):
     if row == n:
         print_board(board)
         return True
@@ -37,18 +32,16 @@ def solve_n_queens(board, row, n):
     result = False
     for col in range(n):
         if is_safe(board, row, col, n):
-            board[row][col] = 'Q'
-            result = solve_n_queens(board, row + 1, n) or result
-            board[row][col] = '.'  # Backtrack
+            board[row][col] = "Q"
+            result = solve_n_queen(board, row+1, n) or result
+            board[row][col] = "."
     return result
 
+def n_queen(n):
+    board= [['.' for _ in range(n)] for _ in range(n)]
+    print_board(board)
+    if not solve_n_queen(board, 0, n):
+        print("No solution exist")
 
-def n_queens(n):
-    board = [['.' for _ in range(n)] for _ in range(n)]
-    # print(board)
-    if not solve_n_queens(board, 0, n):
-        print("No solution exists.")
-
-
-# Example: Solve for N = 4
-n_queens(4)
+n = int(input("Input Number of Queen: "))
+n_queen(n)
